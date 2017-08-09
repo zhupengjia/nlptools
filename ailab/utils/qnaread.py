eda289c55d0d99d677f54a60399329dbb639c55c
Pengjia Zhu <pengjia.zhu@accenture.com>
2017-08-09 12:20:59 +0800

    updated qnaread for subset dropna

--- ailab/utils/qnaread.py
+++ ailab/utils/qnaread.py
@@ -6,15 +6,15 @@ class QnARead:
     def __init__(self, cfg):
         self.cfg = cfg
     
-    def readQnA(self):
+    def readQnA(self, subset=None):
         if self.cfg["question_db_type"] == "mysql":
-            self.readQnA_mysql()
+            self.readQnA_mysql(subset)
         elif self.cfg["question_db_type"] == "xls":
-            self.readQnA_xls()
+            self.readQnA_xls(subset)
         elif self.cfg["question_db_type"] == "csv":
-            self.readQnA_csv()
+            self.readQnA_csv(subset)
 
-    def readQnA_mysql(self, limit = 0, offset = 0):
+    def readQnA_mysql(self, subset=None, limit = 0, offset = 0):
         import MySQLdb
         con = MySQLdb.connect(host = self.cfg["question_host"],\
                 port = self.cfg["question_port"],\
@@ -29,17 +29,17 @@ class QnARead:
         if offset > 0:
             cmd += " offset %i"%offset
         self.data = pandas.read_sql(cmd, con)
-        self.data = self.data[self.cfg['question_header']].dropna()
+        self.data = self.data[self.cfg['question_header']].dropna(subset=subset)
         con.close()
 
-    def readQnA_xls(self):
+    def readQnA_xls(self, subset=None):
         self.data = pandas.read_excel(self.cfg["question_file"], sheetname=self.cfg['question_sheetname'])
         self.data.columns = self.cfg['question_header'] + list(self.data.columns)[len(self.cfg['question_header']):]
-        self.data = self.data[self.cfg['question_header']].dropna()
+        self.data = self.data[self.cfg['question_header']].dropna(subset=subset)
     
-    def readQnA_csv(self):
+    def readQnA_csv(self, subset=None):
         self.data = pandas.read_csv(self.cfg["question_file"], sep=self.cfg["question_sep"])
         self.data.columns = self.cfg['question_header'] + list(self.data.columns)[len(self.cfg['question_header']):]
-        self.data = self.data[self.cfg['question_header']].dropna()
+        self.data = self.data[self.cfg['question_header']].dropna(subset=subset)
 
 
