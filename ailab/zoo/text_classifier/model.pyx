4c0338c277b1c7ba66ab4d28b7a11cf424e8a26d
czx <18516750877@163.com>
2017-12-28 18:40:18 +0800

    add model

---
+++ ailab/zoo/text_classifier/model.pyx
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import tensorflow as tf
+import numpy as np
+import os
+import time
+import datetime
+from tensorflow.contrib import learn
+from ailab.text import Embedding
+from .data_helpers import Data_helpers
+import json
+import pandas as pd
+
+class JudgementModel(object):
+    def __init__(self, cfg={}):
+        self.cfg = cfg
+        self.data_ins = Data_helpers(self.cfg)
+        self.load_checkpoint()
+        
+        self.graph = tf.Graph()
+        
+        with self.graph.as_default():
+            session_conf = tf.ConfigProto(
+				allow_soft_placement=True,
+				log_device_placement=False)
+        
+            self.sess = tf.Session(config = session_conf, graph = self.graph)
+            saver = tf.train.import_meta_graph("{}.meta".format(self.checkpoint_file))
+            saver.restore(self.sess, self.checkpoint_file)
+        
+            self.input_x = self.graph.get_operation_by_name("input_x").outputs[0]
+            self.dropout_keep_prob = self.graph.get_operation_by_name("dropout_keep_prob").outputs[0]	
+
+            self.predictions = self.graph.get_operation_by_name("output/predictions").outputs[0]
+            self.score = self.graph.get_operation_by_name("output/classify_score").outputs[0]
+        
+		
+    
+    def load_checkpoint(self):
+		# read model path directly from cfg
+        if 'model_file' in self.cfg:
+            vocab_path = os.path.join(self.cfg['model_file']['out_dir'], "vocab")
+            self.vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
+            
+            self.checkpoint_file = tf.train.latest_checkpoint(self.cfg['model_file']['checkpoint_dir'])
+        else:
+			# read model path from model_path file created by train process
+            if 'model_path' in self.cfg:
+                with open('model_path.json', 'r') as f:
+                    path_cfg = json.load(f)
+                    path_cfg = json.loads(path_cfg)
+                vocab_path = os.path.join(path_cfg['out_dir'], "vocab")
+                self.vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
+                self.checkpoint_file = tf.train.latest_checkpoint(path_cfg['checkpoint_dir'])
+
+        
+
+    def predict(self, query=''):
+        x_text = [self.data_ins.seg_sentence(query)]
+        x_test = np.array(list(self.vocab_processor.transform(x_text)))
+        
+        # Tensors to evaluate, outputs[0]输出为list
+        self.result, self.scores = self.sess.run([self.predictions, self.score], {self.input_x:x_test, self.dropout_keep_prob: 1.0})	
