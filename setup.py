54db1858c89b8a26811481ea6ac28d039c7d1b74
Pengjia Zhu <pengjia.zhu@accenture.com>
2017-12-29 16:32:25 +0800

    updated

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name = "ailab",
-    version = "0.0.4",
+    version = "0.1.0",
     author = "Pengjia Zhu",
     author_email = "zhupengjia@gmail.com",
     description = ("ailab"),
