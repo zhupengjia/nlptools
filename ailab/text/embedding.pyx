c636baeb903494bf7f1ce01ccadc67af31618880
Pengjia Zhu <pengjia.zhu@accenture.com>
2018-04-30 18:12:40 +0800

    updated embedding

--- ailab/text/embedding.pyx
+++ ailab/text/embedding.pyx
@@ -22,15 +22,17 @@ class Embedding_Base(object):
                     - vec_len: int, vector length, default is 300
                     - vec_type: type of saved vector, default is float64
                     - cached_w2v: the cache path in local, default is ''
+                    - outofvocab: 0 for random, 1 for random but with additional dim, default is 0
                 
     '''
     def __init__(self, cfg):
-        self.cfg = {'vec_len':300, 'vec_type':'float64', 'cached_w2v':''}
+        self.cfg = {'vec_len':300, 'vec_type':'float64', 'cached_w2v':'', 'outofvocab': 0}
         for k in cfg:self.cfg[k] = cfg[k]
         self.__get_cached_vec()
-        self.vec_len = int(self.cfg['vec_len']) + 1
+        if self.cfg['outofvocab']:
+            self.vec_len = int(self.cfg['vec_len']) + 1
+   
 
-    
     def distance(self, word1, word2):
         '''
             Calculate the cosine distance for two words
@@ -49,6 +51,20 @@ class Embedding_Base(object):
         else:
             self.cached_vec = {}
    
+    
+    def _postdeal(self, v = None, returnbase64 = False):
+        if self.cfg['outofvocab']:
+            if v:
+                v = np.concatenate((v, np.zeros(1)))
+            else:
+                v = np.concatenate((np.random.randn(self.vec_len - 1), np.ones(1)))
+        else:
+            if not v:
+                v = np.random.randn(self.vec_len)
+        if returnbase64:
+            v = base64.b64encode(v.tostring()).decode()
+        return v
+    
 
     def save(self):
         '''
@@ -87,12 +103,8 @@ class Embedding_File(Embedding_Base):
         if word in self.cached_vec:
             return self.cached_vec[word]
         self._load_vec()
-        if word in self.word2idx:
-            v = np.concatenate((self.idx2vec[self.word2idx[word]],np.zeros(1)))
-        else:
-            v = np.concatenate((np.random.randn(self.vec_len - 1),np.ones(1)))
-        if 'RETURNBASE64' in self.cfg:
-            v = base64.b64encode(v.tostring()).decode()
+        v = self.idx2vec[self.word2idx[word]] if word in self.word2idx else None
+        v = self._postdeal(v, 'RETURNBASE64' in self.cfg)
         self.cached_vec[word] = v
         return v
 
@@ -127,18 +139,18 @@ class Embedding_Redis(Embedding_Base):
         if word in self.cached_vec:
             return self.cached_vec[word]
         v = self.redis_ins.get(word)
-        if v is not None:
-            v = np.concatenate((np.fromstring(v),np.zeros(1)))
+        if not v:
+            v = self._postdeal(v, 'RETURNBASE64' in self.cfg)
         else:
-            v = np.concatenate((np.random.randn(self.vec_len - 1),np.ones(1))).astype(self.cfg['vec_type'])
+            if not 'RETURNBASE64' in self.cfg:
+                v = self._postdeal(np.fromstring(base64.b64decode(v)), 'RETURNBASE64' in self.cfg)
         self.cached_vec[word] = v
-        if 'RETURNBASE64' in self.cfg:
-            v = base64.b64encode(v.tostring()).decode()
         return v
 
     def __contains__(self, word):
         v = self.redis_ins.get(word)
         return v is not None
+
 
 class Embedding_Random(Embedding_Base):
     '''
@@ -164,14 +176,13 @@ class Embedding_Random(Embedding_Base):
     def __getitem__(self, word):
         if word in self.cached_vec:
             return self.cached_vec[word]
-        v = np.random.randn(self.vec_len)
-        if 'RETURNBASE64' in self.cfg:
-            v = base64.b64encode(v.tostring()).decode()
+        v = self._postdeal(None, 'RETURNBASE64' in self.cfg)
         self.cached_vec[word] = v
         return v
 
     def __contains__(self, word):
         return word in self.cached_vec
+
 
 class Embedding_Dynamodb(Embedding_Base):
     '''
@@ -199,7 +210,9 @@ class Embedding_Dynamodb(Embedding_Base):
         if word in self.cached_vec:
             return self.cached_vec[word]
         v = self.table.get_item(Key={"word":word})
-        if "Item" in v:
+        if not "Item" in v:
+            v = self._postdeal(None, 'RETURNBASE64' in self.cfg)
+        else:
             vector_binary = v['Item']['vector']
             if isinstance(vector_binary, boto3.dynamodb.types.Binary):
                 vector_binary = vector_binary.value
@@ -207,58 +220,7 @@ class Embedding_Dynamodb(Embedding_Base):
                 v = vector_binary
             else:
                 vector = np.fromstring(base64.b64decode(vector_binary), dtype=self.cfg['vec_type'])
-                v = np.concatenate((vector, np.zeros(1)))
-        else:
-            v = np.concatenate((np.random.randn(self.vec_len - 1),np.ones(1))).astype(self.cfg['vec_type'])
-            if 'RETURNBASE64' in self.cfg:
-                v = base64.b64encode(v.tostring()).decode()
-        self.cached_vec[word] = v
-        return v
-
-    def __contains__(self, word):
-        v = self.table.get_item(Key={"word":word})
-        return "Item" in v
-
-
-class Embedding_Rest(Embedding_Base):
-    '''
-        Read the word vectors from restapi
-
-        Input:
-            - cfg: dictionary or ailab.utils.config object
-                - needed keys:
-                    - vec_type: the type of vector used in restapi, float64, float32, float
-                    - embedding_restapi: string, restapi url 
-                    - RETURNBASE64: if this config exists, will return BASE64 instead of vector
-
-        Usage:
-            - emb_ins[word]: return the vector or BASE64 format vector
-            - word in emb_ins: check if word available in restapi
-    '''
-    def __init__(self, cfg):
-        import boto3
-        Embedding_Base.__init__(self, cfg)
-        self.client = boto3.resource('dynamodb')
-        self.table = self.client.Table(cfg['dynamodb'])
-
-    def __getitem__(self, word):
-        import boto3
-        if word in self.cached_vec:
-            return self.cached_vec[word]
-        v = self.table.get_item(Key={"word":word})
-        if "Item" in v:
-            vector_binary = v['Item']['vector']
-            if isinstance(vector_binary, boto3.dynamodb.types.Binary):
-                vector_binary = vector_binary.value
-            if 'RETURNBASE64' in self.cfg:
-                v = vector_binary
-            else:
-                vector = np.fromstring(base64.b64decode(vector_binary), dtype=self.cfg['vec_type'])
-                v = np.concatenate((vector, np.zeros(1)))
-        else:
-            v = np.concatenate((np.random.randn(self.vec_len - 1),np.ones(1))).astype(self.cfg['vec_type'])
-            if 'RETURNBASE64' in self.cfg:
-                v = base64.b64encode(v.tostring()).decode()
+                v = self._postdeal(vector)
         self.cached_vec[word] = v
         return v
 
@@ -288,13 +250,10 @@ class Embedding_Rest(Embedding_Base):
     def __getitem__(self, word):
         if word in self.cached_vec:
             return self.cached_vec[word]
-        vector_binary = restpost(self.rest_url, {'text':word})
-        if 'RETURNBASE64' in self.cfg:
-            self.cached_vec[word] = vector_binary
-            return vector_binary
-        vector = np.fromstring(base64.b64decode(vector_binary), dtype=self.cfg['vec_type'])
-        if len(vector) == self.vec_len - 1:
-            vector = np.concatenate((vector, np.zeros(1))).astype(self.cfg['vec_type'])
+        vector = restpost(self.rest_url, {'text':word})
+        if not 'RETURNBASE64' in self.cfg:
+            vector = np.fromstring(base64.b64decode(vector), dtype=self.cfg['vec_type'])
+            vector = self._postdeal(vector)
         self.cached_vec[word] = vector
         return vector
 
