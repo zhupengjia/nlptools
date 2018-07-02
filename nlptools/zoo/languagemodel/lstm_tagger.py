61ea38f03dfea4a40448845e9672b1c2df7667f2
Pengjia Zhu <zhupengjia@gmail.com>
2018-07-02 21:02:43 +0800

    updated

---
+++ nlptools/zoo/languagemodel/lstm_tagger.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from ..reader.bucket import BucketData, prepare_sequence
+from ..reader.bucket import PAD_ID
+
+
+def to_one_hot(inputs, num_class):
+    one_hot = torch.zeros(inputs.shape + (num_class,))
+    index = inputs.view(inputs.shape + (1,))
+    expand_dim = len(index.shape) - 1
+    one_hot.scatter_(expand_dim, index, 1.)
+    return one_hot
+
+
+class LSTMTagger(nn.Module):
+    
+    def __init__(self, 
+                 embedding_dim, hidden_dim, 
+                 vocab_size, tagset_size,
+                 num_layers, batch_size=1, 
+                 bucket_config=None, device=None):
+        
+        super(LSTMTagger, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.device = device
+        self.bucket_config = bucket_config
+        
+        self.vocab_size = vocab_size
+        self.tagset_size = tagset_size
+        
+        self.num_layers = num_layers
+        self.batch_size = batch_size
+        
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self.hidden = self.init_hidden()
+        
+        
+    def init_hidden(self):
+        # the hidden state should have the shape of 
+        #  (num_layer*num_dirction, batch, hid_dim*num_direction)
+        # and don't forget the extra cell-state
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim).to(self.device),
+               torch.zeros(self.num_layers, self.batch_size, self.hidden_dim).to(self.device))
+        
+
+    def forward(self, sentence):
+        """
+            Args: 
+                sentence: The input sentence, word idx not mapped into embedding space, in the shape of `(batch?, len)`
+                `nn.Embedding` would accept any shape tensor with value type Long 
+        """
+        embeds = self.word_embeddings(sentence)
+        # now make the len to be inferred
+        lstm_out, self.hidden = self.lstm(
+            embeds.view(self.batch_size, -1, self.embedding_dim), self.hidden)
+        
+        # generate the logits of the tags
+        logits = lstm_out.view(self.batch_size, -1, self.embedding_dim)
+        tag_space = self.hidden2tag(logits)
+
+        # then squash into the probabiliy score
+        # meaning of the dims: (len, batch, emb)
+        tag_scores = F.log_softmax(tag_space, dim=2)
+        return tag_scores
+    
+    
+    def sequence_loss(self, inputs, targets):
+        loss_function = nn.NLLLoss(ignore_index=PAD_ID)
+        targets_one_hot = to_one_hot(targets, self.tagset_size)
+        outputs = self(inputs).view(targets_one_hot.shape)
+        xent = outputs * targets_one_hot
+        xent = xent.sum(dim=2, keepdim=False)
+        return xent
+            
+    
+
+    def train(self, inputs, targets, num_epoch=20, save_path='autosave.torch'):
+        # NLL is equivalent to the multi-category cross-entropy.
+        loss_function = nn.NLLLoss(ignore_index=PAD_ID)
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+    
+        for epoch in range(num_epoch):
+            #if epoch % 10 == 0: 
+            print('Starting epoch {}'.format(epoch))
+                
+            buckets = BucketData(inputs, targets, self.bucket_config)
+            for batch_inputs, batch_tags in buckets:
+                self.zero_grad()
+                self.hidden = self.init_hidden()
+               
+                batch_inputs = batch_inputs.to(self.device)
+                batch_tags = batch_tags.to(self.device)
+
+                #print('batch_inputs', batch_inputs.size())
+                #print('tags', batch_tags.size())
+                
+                tag_scores = self(batch_inputs)
+                
+                tag_scores_flatten = tag_scores.view(-1, self.tagset_size)
+                targets_flatten = batch_tags.view(-1)
+                
+                loss = loss_function(tag_scores_flatten, targets_flatten)
+                loss.backward()
+                optimizer.step()
+            #if epoch % 10 == 0: 
+            print('Epoch loss {}'.format(loss))
+            torch.save(self.state_dict(), save_path)
+                
+                
+    def load_params(self, save_path):
+        self.load_state_dict(torch.load(save_path))
+
+
+
+    
+def main():
+    from bucket import demo_data, prepare_lm_data
+
+    '''
+    PART I. Training
+    '''
+    
+    inputs, targets, vocab_size, tagset_size = demo_data()
+    inputs, targets = prepare_lm_data(inputs)
+    print('vocab_size: {}, tagset_size: {}'.format(vocab_size, tagset_size))
+    print('data before trainig:')
+    print(inputs)
+    print(targets)
+    
+    model = LSTMTagger(
+            embedding_dim=8, hidden_dim=8, 
+            # VERY IMPORTANT! to use the vocab_size as the tagset_size
+            vocab_size=vocab_size, tagset_size=vocab_size,
+            num_layers=1
+    )
+    
+    '''
+        `bucket_config = [(4, 3), (8, 2)]` means that:
+        batch_size for seq_len up to 4 is 3
+        batch_size for seq_len up to 8 is 2
+        batch_size for seq_len more than 8 is 1
+    '''
+    
+    model.bucket_config = [(4, 3), (8, 2)]
+    model.train(inputs, targets, num_epoch=200)
+    
+    '''
+    PART II. TESTING
+    '''
+    
+    model = LSTMTagger(
+            embedding_dim=8, hidden_dim=8, 
+            # VERY IMPORTANT! to use the vocab_size as the tagset_size
+            vocab_size=vocab_size, tagset_size=vocab_size,
+            num_layers=1
+    )
+    model.load_params('autosave.torch')
+    
+    inputs, targets, vocab_size, tagset_size = demo_data()
+    inputs, targets = prepare_lm_data(inputs)
+    
+    buckets = BucketData(inputs, targets, [(4, 3), (8, 2)])
+    for batch_inputs, batch_tags in buckets:
+        xent = model.sequence_loss(batch_inputs, batch_tags)
+        print('batch inputs:', batch_inputs)
+        print('cross entropy:', xent)
+        print('-' * 20)
+    
+    
+if __name__ == '__main__':
+    main()
+
---
+++ nlptools/zoo/modules/bucket.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+PAD_ID = 0
+PAD_TAG = '<PAD>'
+SOS_ID = 1
+SOS_TAG = '<SOS>'
+EOS_ID = 2
+EOS_TAG = '<EOS>'
+
+
+def prepare_sequence(seq, to_ix, batch_mode=False):
+    '''
+    Args:
+        seq: the input sequence
+        to_ix: the dictionary
+        batch_mode: if the input sequence is are batches
+    '''
+    if batch_mode:
+        return np.asarray([prepare_sequence(_, to_ix) for _ in seq], dtype=np.object)
+    return torch.tensor([to_ix[w] for w in seq], dtype=torch.long)
+
+
+
+def prepare_lm_data(inputs):
+
+    #print(inputs)
+    #print('>>')
+    def shift(input, offset, pad):
+        padded = input.new_zeros(len(input) + abs(offset))
+        #padded = np.zeros(len(input) + abs(offset),'int')
+        if offset > 0:
+            padded[offset:] = input
+            padded[:offset] = pad
+        else:
+            padded[:len(input)] = input
+            padded[len(input):] = pad
+        return padded
+    
+    ans = [(shift(input, 1, SOS_ID), shift(input, -1, EOS_ID)) \
+            for input in inputs]
+    inputs, targets = zip(*ans)
+    return np.array(inputs, dtype=np.object),\
+            np.array(targets, dtype=np.object)
+
+
+class BucketData:
+
+
+    def __init__(self, inputs, tags, bucket_config):
+        """
+        Args:
+            data: list of inputs, should be in the shape of (num_instance, time_step).
+              However, usually the input is not an numpy nd-array, and usually is a local batch 
+              of the entire dataset. 
+            bucket_config: e.g., `{4: 3, 8: 2}` means for inputs of length up to 4, 
+              the batch size will be 3, and for inputs of length up to 8, the batch
+              size will be 2, for inputs of length more than 8, the batch size will be 1.
+        """
+        # build a list of lengths for the input data
+        self.len_list = np.asarray(list(map(len, inputs)), dtype=np.integer)
+        # save the sort_idx such that the original input order can be preserved. 
+        self.sort_idx = np.argsort(self.len_list)
+        
+        # now, let's sort the length
+        self.sorted_len = self.len_list[self.sort_idx]
+        
+        #self.input_data = np.asarray(data)
+        #self.input_tags = np.asarray(tags)
+
+        self.sorted_inputs = inputs[self.sort_idx]
+        self.sorted_tags = tags[self.sort_idx]
+        self.bucket_config = bucket_config
+        
+        # the actual work goes here.  
+        self.build_buckets()
+        
+    
+    def build_buckets(self):
+        self.buckets = []
+        i, j = 0, 0
+        for max_seq_len, batch_size in self.bucket_config:
+            # keep marching through the length-sorted input data
+            while j < len(self.sorted_len):
+                j = j + 1
+                # until hit the first item longer than the `max_seq_len`
+                if self.sorted_len[j - 1] > max_seq_len:
+                    self.buckets.append((self.sorted_inputs[i:j - 1],
+                                         self.sorted_tags[i:j - 1]))
+                    i = j - 1
+                    break
+        # there is one more bucket that has everything left
+        self.buckets.append((np.asarray(self.sorted_inputs[j - 1:]), 
+                            np.asarray(self.sorted_tags[j - 1:])))
+        
+        
+    def __iter__(self):
+        for bucket_idx, bucket in enumerate(self.buckets):
+            # for a bucket whose batch_size is defined in the config,
+            # we use the defined batch_size
+            if bucket_idx < len(self.bucket_config):
+                (max_seq_len, batch_size) = self.bucket_config[bucket_idx]
+                for batch in BucketData.build_batch_from_bucket(bucket, batch_size):
+                    yield batch
+                    
+            # otherwise, we have one element each batch
+            else:
+                bucket_inputs, bucket_tags = bucket
+                for i in range(len(bucket_inputs)):
+                    yield bucket_inputs[i].view(1, -1), \
+                            bucket_tags[i].view(1, -1)
+
+                
+    @staticmethod
+    def build_batch_from_bucket(bucket, batch_size):
+        bucket_inputs, bucket_tags = bucket
+        for batch_start in range(0, len(bucket_inputs), batch_size):
+            batch_inputs = bucket_inputs[batch_start : batch_start + batch_size][::-1]
+            batch_tags = bucket_tags[batch_start : batch_start + batch_size][::-1]
+            batch_inputs = nn.utils.rnn.pad_sequence(
+                    batch_inputs, batch_first=True, padding_value=0)
+            batch_tags = nn.utils.rnn.pad_sequence(
+                    batch_tags, batch_first=True, padding_value=0)
+            yield batch_inputs, batch_tags
+        
+        
+    def restore(self, sequence):
+        pass
+    
+
+def demo_data():
+    
+    training_data = [
+        ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+        ("Everybody read that book".split(), ["NN", "V", "DET", "NN"]),
+        ("The dog".split(), ["DET", "NN"]),
+        ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+        ("Everybody read that book".split(), ["NN", "V", "DET", "NN"]),
+        ("Everybody read that book Everybody read that book".split(), ["NN", "V", "DET", "NN", "NN", "V", "DET", "NN"]),
+        ("Everybody read that book The dog ate the apple".split(), ["NN", "V", "DET", "NN", "DET", "NN", "V", "DET", "NN"]),
+        ("Everybody read that book".split(), ["NN", "V", "DET", "NN"]),
+        ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+        ("Everybody read that book Everybody read that book".split(), ["NN", "V", "DET", "NN", "NN", "V", "DET", "NN"])
+    ]
+
+
+    word_to_ix = {PAD_TAG:PAD_ID, SOS_TAG:SOS_ID, EOS_TAG:EOS_ID}
+    for sent, tags in training_data:
+        for word in sent:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+
+    tag_to_ix = {PAD_TAG: PAD_ID, SOS_TAG:SOS_TAG, EOS_TAG: EOS_ID, "DET": 3, "NN": 4, "V": 5}
+    inputs, tags = zip(*training_data)
+    
+    inputs = prepare_sequence(inputs, word_to_ix, True)
+    tags = prepare_sequence(tags, tag_to_ix, True)
+    
+    return inputs, tags, len(word_to_ix), len(tag_to_ix)
+    
+    
+if __name__ == '__main__':
+    inputs, tags, _, _ = demo_data()
+    print(inputs)
+    print(tags)
+
+    data = BucketData(inputs, tags, [(4, 3), (8, 2)])
+    for batch_inputs, batch_tags in data:
+        print('>> batch inputs: ', batch_inputs)
+        print('>> batch tags: ', batch_tags)
