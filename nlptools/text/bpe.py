4a84cb70ad4653bc08eff3ebe2bcfeca99d9c9d3
Pengjia Zhu <zhupengjia@gmail.com>
2018-08-07 12:44:05 +0800

    small update

--- nlptools/text/bpe.py
+++ nlptools/text/bpe.py
@@ -13,7 +13,7 @@ class BytePair(Vocab):
 
     def __init__(self, bpe_size=1000, min_freq=2, separator='@@', code_file='', **args):
         '''
-            Byte Pair Encoding (BPE) vocabulary for rare word, see https://arxiv.org/abs/1508.07909. Only used for latin languages. Note the bpe vocab is included in total vocab
+            Byte Pair Encoding (BPE) vocabulary for rare word, see https://arxiv.org/abs/1508.07909. Note the bpe vocab is included in total vocab
 
             Input:
                 - min_freq: int, Stop if no symbol pair has frequency >= min_freq, default is 2
