76e0d480f1ae7366c03a9dea54843636e3ee09b6
Pengjia Zhu <zhupengjia@gmail.com>
2019-07-26 15:05:49 +1200

    updated text

--- nlptools/text/__init__.py
+++ nlptools/text/__init__.py
@@ -6,10 +6,11 @@ from .tokenizer import Tokenizer
 from .embedding import Embedding
 from .vocab import Vocab
 from .translate import Translate
+from .tfidf import TFIDF
 from .vectfidf import VecTFIDF
 from .synonyms import Synonyms
 from .annoysearch import AnnoySearch
 from .acorasearch import AcoraSearch
 from .ner import NER
 
-__all__ = ["Tokenizer", 'Embedding', 'Vocab', 'VecTFIDF', 'Synonyms', 'AnnoySearch', 'AcoraSearch', 'Translate', 'NER']
+__all__ = ["Tokenizer", 'Embedding', 'Vocab', 'TFIDF', 'VecTFIDF', 'Synonyms', 'AnnoySearch', 'AcoraSearch', 'Translate', 'NER']
